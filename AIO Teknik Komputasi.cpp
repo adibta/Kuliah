b9ff11c4aec3ea445ed155c65e40948332aec1d5
adibta <adibreally@gmail.com>
2016-05-13 13:26:53 +0700

    Add files via upload
    Penambahan error tolerance + metode sampai secant

--- AIO Teknik Komputasi.cpp
+++ AIO Teknik Komputasi.cpp
@@ -8,7 +8,8 @@ class func
 	public:
 		double a,b,c;
 		void set_value(double,double,double);
-		double result(double);
+		double fresult(double);
+		double ffresult(double);
 };
 
 void func::set_value(double x,double y,double z)
@@ -18,10 +19,16 @@ void func::set_value(double x,double y,double z)
 	c = z;
 }
 
-double func::result(double nil_x)
+double func::fresult(double nil_x)
 {
 	return(a*pow(nil_x,2)+b*nil_x+c);
 }
+
+double func::ffresult(double nil2_x)
+{
+	return(2*a*nil2_x+b);
+}
+
 
 void input_value(func& pers)
 {
@@ -42,40 +49,36 @@ void bisection()      //Terbukti valid
 {
 	func persamaan;
 	double xu,xl,xr;
-	int iter;
+	int counter=1;
 	input_value(persamaan);
-	cout<<"#Metode Bisection#"<<endl;
+	cout<<"#Metode Bisection# "<<endl;
+	cout<<"||Toleransi Error =  0.00001||"<<endl;
 	cout<<"Tentukan Batas atas dan batas bawah"<<endl;
 	cout<<"Batas atas = ";
 	cin>>xu;
 	cout<<"Batas bawah = ";
 	cin>>xl;
-	cout<<"Jumlah iterasi = ";
-	cin>>iter;
-	if (persamaan.result(xu)*persamaan.result(xl) <= 0 )
+	if (persamaan.fresult(xu)*persamaan.fresult(xl) <= 0 )
 	{
-		for (int j = 0;j<iter;j++)
+		while (true)
 		{
-			cout<<"##Iterasi ke-"<<j+1<<"##"<<endl;
+			cout<<"##Iterasi ke-"<<counter<<"##"<<endl;
 			cout<<"Batas atas = "<<xu<<endl;
 			cout<<"Batas bawah = "<<xl<<endl;
 			xr = (xu + xl)/2;
 			cout<<"Nilai tengah = "<<xr<<endl;
-			if (persamaan.result(xr) == 0)
+			if (abs(persamaan.fresult(xr)) < 0.00001)
 			{
 				cout <<"akar persamaannya adalah : "<<xr<<endl;
-				cout <<"Operasi Bisection berhenti pada iterasi ke-"<<j+1<<endl;
+				cout <<"Operasi Bisection berhenti pada iterasi ke-"<<counter<<endl;
 				break;
 			}
-			else if (persamaan.result(xl)*persamaan.result(xr) < 0)
+			else if (persamaan.fresult(xl)*persamaan.fresult(xr) < 0)
 				xu = xr;
 			else
 				xl = xr;
-			if ((j == iter-1)&&(persamaan.result(xr) != 0))
-			{
-				cout<<"Akar persamaan hasil pendekatan dengan "<<j+1<<" iterasi adalah : "<<xr<<endl;
-			}
 			cout <<endl;
+			counter++;
 		}
 	}
 	else 
@@ -86,58 +89,115 @@ void falsePosition()  //Belum Dibuktikan
 {
 	func persamaan;
 	double xu,xl,xr;
-	int iter;
+	int counter;
 	input_value(persamaan);
 	cout<<"#Metode False Position#"<<endl;
+	cout<<"||Toleransi Error =  0.00001||"<<endl;
 	cout<<"Tentukan Batas atas dan batas bawah (f(xl) harus < 0 dan f(xu) harus > 0)"<<endl;
 	cout<<"Batas atas = ";
 	cin>>xu;
 	cout<<"Batas bawah = ";
 	cin>>xl;
-	cout<<"Jumlah iterasi = ";
-	cin>>iter;
-	if ((persamaan.result(xu))>0 && (persamaan.result(xl) <0) )
+	if ((persamaan.fresult(xu))>0 && (persamaan.fresult(xl) <0) )
 	{
-		for (int j = 0;j<iter;j++)
+		while (true)
 		{
-			cout<<"##Iterasi ke-"<<j+1<<"##"<<endl;
+			cout<<"##Iterasi ke-"<<counter<<"##"<<endl;
 			cout<<"Batas atas = "<<xu<<endl;
 			cout<<"Batas bawah = "<<xl<<endl;
-			xr = (xl*persamaan.result(xu)-xu*persamaan.result(xl))/(persamaan.result(xu)-persamaan.result(xl));
+			xr = (xl*persamaan.fresult(xu)-xu*persamaan.fresult(xl))/(persamaan.fresult(xu)-persamaan.fresult(xl));
 			cout<<"Nilai tengah = "<<xr<<endl;
-			if (persamaan.result(xr) == 0)
+			if (abs(persamaan.fresult(xr)) < 0.00001)
 			{
 				cout <<"akar persamaannya adalah : "<<xr<<endl;
-				cout <<"Operasi False Position berhenti pada iterasi ke-"<<j+1<<endl;
+				cout <<"Operasi False Position berhenti pada iterasi ke-"<<counter<<endl;
 				break;
 			}
-			else if (persamaan.result(xr) < 0)
+			else if (persamaan.fresult(xr) < 0)
 				xl = xr;
 			else
 				xu = xr;
-			if ((j == iter-1)&&(persamaan.result(xr) != 0))
-			{
-				cout<<"Akar persamaan hasil pendekatan dengan "<<j+1<<" iterasi adalah : "<<xr<<endl;
-			}
 			cout <<endl;
+			counter++;
 		}
 	}
 	else 
 		cout<< "Batas bawah atau batas atas tidak memenuhi persyaratan"<<endl;
 }
 
+void newtonraphson()
+{
+	func persamaan;
+	double x,xi,xiplus;
+	int counter=1;
+	input_value(persamaan);
+	cout<<"#Metode Newton-Raphson#"<<endl;
+	cout<<"||Toleransi Error =  0.00001||"<<endl;
+	cout<<"Tentukan nilai x awal (bebas)"<<endl;
+	cout<<"x = ";
+	xi = x;
+	cin>>x;
+	while (true)
+	{
+		cout<<"##Iterasi ke-"<<counter<<"##"<<endl;
+		cout<<"x(i) = "<<xi<<endl;
+		xiplus = xi - (persamaan.fresult(xi)/persamaan.ffresult(xi));
+		cout<<"x(i+1) = "<<xiplus<<endl;
+		xi = xiplus;
+		if (persamaan.fresult(xi) < 0.00001)
+		{
+			cout <<"akar persamaannya adalah : "<<xi<<endl;
+			cout <<"Operasi Newton-Raphson berhenti pada iterasi ke-"<<counter<<endl;
+			break;
+		}
+		counter++;
+	}
+}
+void secant()
+{
+	func persamaan;
+	double x1,x2,xi,xiplus,ximin;
+	int counter=1;
+	input_value(persamaan);
+	cout<<"#Metode Secant#"<<endl;
+	cout<<"||Toleransi Error =  0.00001||"<<endl;
+	cout<<"Tentukan nilai x0 dan x1 awal (bebas)"<<endl;
+	cout<<"x0 = ";
+	cin>>ximin;
+	cout<<"x1 = ";
+	cin>>xi;
+	while (true)
+	{
+		cout<<"##Iterasi ke-"<<counter<<"##"<<endl;
+		cout<<"x(i-1) = "<<ximin<<endl;
+		cout<<"x(i) = "<<xi<<endl;
+		xiplus = xi - (persamaan.fresult(xi)*((xi-ximin)/(persamaan.fresult(xi)-persamaan.fresult(ximin))));
+		cout<<"x(i+1) = "<<xiplus<<endl;
+		ximin = xi;
+		xi = xiplus;
+		if (persamaan.fresult(xi) < 0.00001)
+		{
+			cout <<"akar persamaannya adalah : "<<xi<<endl;
+			cout <<"Operasi Secant Method berhenti pada iterasi ke-"<<counter<<endl;
+			break;
+		}
+		counter++;
+	}
+}
 //Main
 int main()
 {
 	int pilihan;
 	cout<< "ALL IN ONE SOLUTION TEKNIK KOMPUTASI"<<endl<<"PILIH METODE"<<endl;
-	cout<< "1. Bisection\n2. False Position";
+	cout<< "1. Bisection\n2. False Position\n3. Newton-Raphson\n4. Secant";
 	cout<<endl;
 	cin>>pilihan;
 	switch (pilihan)
 	{
 		case 1: bisection();
 		case 2: falsePosition();
+		case 3: newtonraphson();
+		case 4: secant();
 	}
 	return 0;
 }
